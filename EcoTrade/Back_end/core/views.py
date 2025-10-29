a0fc9cbed25f88aeeb9479f91c7164b4f8ec3a95
Wandemberg-De-Medeiros <wandembergdemedeiros@gmail.com>
2025-10-29 17:39:49 -0300

    Eco Trade V3

--- EcoTrade/Back_end/core/views.py
+++ EcoTrade/Back_end/core/views.py
@@ -91,12 +91,22 @@ def cadastro_view(request):
         senha = request.POST.get('senha')
         tipo = request.POST.get('tipo-usuario')
         nome = request.POST.get('nome-completo')
+        documento = request.POST.get('documento')
+        cidade = request.POST.get('cidade')
+        estado = request.POST.get('estado')
         
         if email in USUARIOS_DB:
             return render(request, 'cadastro.html', {'mensagem_erro': "E-mail já cadastrado."})
         
         # Adiciona novo usuário à simulação de BD
-        USUARIOS_DB[email] = {"senha": senha, "tipo": tipo, "nome": nome}
+        USUARIOS_DB[email] = {
+            "senha": senha, 
+            "tipo": tipo, 
+            "nome": nome,
+            "documento": documento,
+            "cidade": cidade,
+            "estado": estado
+        }
         
         return redirect(reverse('login'))
         
@@ -104,8 +114,16 @@ def cadastro_view(request):
 
 # Rota /registro_creditos/ (REQUISITO 2)
 def registro_creditos_view(request):
-    if not request.session.get('logged_in') or request.session['user_info']['tipo'] != 'Produtor Rural':
+    if not request.session.get('logged_in'):
         return redirect(reverse('login'))
+        
+    user_info = request.session['user_info']
+
+    if user_info['tipo'] != 'Produtor Rural':
+        context = {
+        'user_nome': user_info['nome'],
+        'user_tipo': user_info['tipo'],
+    }
 
     if request.method == 'POST':
         quantidade = request.POST.get('quantidade')
@@ -130,10 +148,29 @@ def registro_creditos_view(request):
 def transacoes_view(request):
     if not request.session.get('logged_in'):
         return redirect(reverse('login'))
-    
+    user_info = request.session['user_info']
+
+    if request.method == 'POST' and user_info['tipo'] == 'Produtor Rural':
+
+        volume = request.POST.get('volume')
+        preco_unitario = request.POST.get('preco_unitario')
+        
+        nova_oferta = {
+            "id": str(uuid.uuid4())[:6], 
+            "produtor": user_info['nome'],
+            "origem": f"Venda Rápida - {user_info['nome']}",
+            "quantidade": int(volume),
+            "preco_un": float(preco_unitario),
+        }
+        CREDITOS_DISPONIVEIS.append(nova_oferta)
+        print("Nova Oferta de Venda Criada:", nova_oferta)
+        mensagem_sucesso = "Seus créditos foram listados com sucesso no Marketplace!"
+
     context = {
+       'user_nome': user_info['nome'],
+        'user_tipo': user_info['tipo'],
         'ofertas': CREDITOS_DISPONIVEIS,
-        'user_tipo': request.session['user_info']['tipo']
+        'mensagem_sucesso': mensagem_sucesso if 'mensagem_sucesso' in locals() else None,
     }
     
     return render(request, 'transacoes.html', context)
